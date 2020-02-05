a12d5eb9eedf46a7b9b2489478ba75a93350fef3
AspectoX <aspectox.creativa@gmail.com>
2020-02-05 08:35:30 -0300

    commit inicio

---
+++ js/ax.js
@@ -0,0 +1,35 @@
+// Menu principal
+const contenedor = document.querySelector('#contenedor');
+
+document.querySelector('#boton-menu').addEventListener('click', () => {
+    contenedor.classList.toggle('active');
+});
+// / Fin Menu principal
+
+// Panel toolbox
+$(document).ready(function() {
+    $('.collapse-link').on('click', function() {
+        var $BOX_PANEL = $(this).closest('.x_panel'),
+            $ICON = $(this).find('i'),
+            $BOX_CONTENT = $BOX_PANEL.find('.x_content');
+
+        // fix for some div with hardcoded fix class
+        if ($BOX_PANEL.attr('style')) {
+            $BOX_CONTENT.slideToggle(200, function() {
+                $BOX_PANEL.removeAttr('style');
+            });
+        } else {
+            $BOX_CONTENT.slideToggle(200);
+            $BOX_PANEL.css('height', 'auto');
+        }
+
+        $ICON.toggleClass('ax-chevron-up ax-chevron-down');
+    });
+
+    $('.close-link').click(function() {
+        var $BOX_PANEL = $(this).closest('.x_panel');
+
+        $BOX_PANEL.remove();
+    });
+});
+// /Fin Panel toolbox
